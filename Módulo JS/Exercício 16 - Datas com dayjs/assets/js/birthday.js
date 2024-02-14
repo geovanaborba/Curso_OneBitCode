8af727e547380b3fcf653e4b95a20a3604dc1385
Geovana Borba <geborba19@hotmail.com>
2024-02-14 18:54:50 -0300

    exercício 16 finalizado

---
+++ Módulo JS/Exercício 16 - Datas com dayjs/assets/js/birthday.js
@@ -0,0 +1,21 @@
+const dayjs = require('dayjs');
+
+function birthday(date) {
+    const birthday = dayjs(date); //cria objeto dayjs
+    const today = dayjs(); //valor padrão quando não colocado nada entre parenteses é a data atual do sistema
+
+    const ageInYears = today.diff(birthday, 'year'); //método diff conforme documentação, indica a diferença entre duas datas e horas na unidade especificada. Teremos a diferença da data atual até a unidade definida em birthday. Essa diferença será exibida em anos, como definido entre as aspas simples. (https://day.js.org/docs/en/display/difference)
+
+    const nextBirthday = birthday.add(ageInYears + 1, 'year'); //método add soma com outra unidade definida. Nesse caso, pegaremos o valor da variável de idade e somaremos com mais 1 ano, para definir a data do próximo aniversário (https://day.js.org/docs/en/manipulate/add)
+
+    const daysToNextBirthday = nextBirthday.diff(today, 'day') + 1; //mostrará diferença em dias da data atual até o próximo aniversário.
+
+    console.log(`Idade: ${ageInYears}`);
+    console.log(`Próximo aniversário: ${nextBirthday.format('ddd, DD MMMM YYYY')}`); //método format muda o formato de data exibida (https://day.js.org/docs/en/display/format)
+    console.log(`Faltam ${daysToNextBirthday} dias para completar ${ageInYears + 1} anos.`);
+    //Obs.: caso falte horas para o próximo aniversário, (por exemplo, se o aniversário é amanhã) no terminal seria indicado que faltam 0 dias para o aniversário.Faz-se necessário corrigir isso utilizando a quantidade 1 para incluir o dia de hoje na contagem. Esse mais 1, deve ser incluído na variável daysToNextBirthday
+};
+
+
+
+birthday('1994-12-19'); //ano precisa estar em formato americano
---
+++ Módulo JS/Exercício 16 - Datas com dayjs/readme.md
@@ -0,0 +1,38 @@
+## Datas com dayjs
+
+Nesse exercício você irá treinar uma habilidade muito importante para qualquer programador: A capacidade de aprender a utilizar bibliotecas de terceiros.
+
+• Você deverá criar uma função JavaScript que recebe como parâmetro a data de nascimento de uma pessoa no formato string.
+
+• Mostre no terminal a idade dessa pessoa no dia atual 
+
+• Sua próxima data de aniversário no formato "DD/MM/AAAA" e 
+
+• Quantos dias faltam para o próximo aniversário dela. 
+
+**Para isso, você deverá utilizar o módulo dayjs, disponível no npm.**
+
+<br>
+<hr>
+<br>
+
+Para aprender a utilizar o dayjs, você deverá acessar o site do npm (https://npmjs.com/) e pesquisar por esse módulo.
+
+A página da dayjs no npm irá mostrar como utilizar essa biblioteca. 
+
+Você também deve consultar a documentação do dayjs, disponível em (https://day.js.org/).
+
+<br>
+<hr>
+<br>
+
+Nessa aplicação você não utilizará o browser. Você deverá utilizar o **Node.js**. 
+Para executar seu arquivo com o Node.js basta acessar a pasta onde ele está localizado com o terminal e digitar o comando *node seu_arquivo.js*. 
+
+Além disso, você deverá utilizar a sintaxe dos módulos CommonJS para trabalhar com o dayjs, ou seja, a função require().
+
+**Dica:** Para resolver o exercício, tente procurar pelos métodos de adicionar um intervalo de tempo a uma data (add), calcular a diferença entre uma data e outra (diff) e exibir uma data em um formato específico (format).
+
+<br>
+
+## [🚀 Ver resultado no GitHub Pages](https://geovanaborba.github.io/Curso_OneBitCode/M%C3%B3dulo%20JS/Exercício%2016%20-%20Datas%20com%20dayjs)
